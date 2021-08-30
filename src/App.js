c467ff47b74d4ad71dfe0786230ca9777f8e1990
jmarellanes <josemanuel.arellanes@gmail.com>
2021-08-30 02:35:45 -0600

    1. Add & Build > _abstracts.scss - Add Sass functions to calculate: px to rem; px value to line-height and rem to px. 2. Add & Build > _base.scss - Reset rules, general rules and property to hidden content with accesibility. 3. Add & Build: _typography.scss > Add font files to project; Add font-face rules; Add typography rules to body and headings. 4. Add & Build: _variables.scss > Add CSS custom properties for: dafault spacers and fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import DesignSystem from './pages/DesignSystem';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <div>Feedback Board</div>
+    <div className='App'>
+      <div className='title'>Feedback Board</div>
+      <DesignSystem />
     </div>
   );
 }
---
+++ src/pages/DesignSystem.js
@@ -0,0 +1,32 @@
+export default function DesignSystem() {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At, corrupti
+        deserunt.
+      </h1>
+      <h2>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum explicabo
+        cupiditate corrupti voluptas, iusto.
+      </h2>
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At, corrupti
+        deserunt.
+      </h3>
+      <h4>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum explicabo
+        cupiditate corrupti voluptas, iusto.
+      </h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        facere sed eius, deserunt, nesciunt unde harum distinctio illum
+        consequuntur id maiores accusamus nam magnam tempora numquam voluptas
+        soluta mollitia praesentium! Nesciunt, enim repudiandae modi placeat
+        iste ab, odit cupiditate voluptate officiis laborum architecto
+        voluptatem quasi cum assumenda error expedita. Eveniet doloribus numquam
+        necessitatibus exercitationem maxime culpa architecto repellendus amet
+        aspernatur.
+      </p>
+    </div>
+  );
+}
