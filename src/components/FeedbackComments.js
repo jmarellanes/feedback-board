3a932fe14f884d48133d416fc4b755b337e124df
jmarellanes <josemanuel.arellanes@gmail.com>
2021-11-07 20:27:13 -0700

    Build > Grid Layout for feedback commetns

--- src/components/FeedbackComments.js
+++ src/components/FeedbackComments.js
@@ -7,7 +7,7 @@ function FeedbackComments({
   commentId,
   replyComment,
 }) {
-  let replyComments = () =>
+  let replyCommentsList = () =>
     allComments.filter((c) => c.fields.ParentId === commentId);
 
   return (
@@ -17,26 +17,28 @@ function FeedbackComments({
           replyComment ? 'feedback-comment--reply' : ''
         }`}
       >
-        <header className='feedback-comment__info'>
-          <h4>{name}</h4>
-          <p>{username}</p>
-        </header>
-        <div className='feedback-comment__author-image'>
-          <img src={image[0].url} alt='' />
+        <div className='feedback-comment__container'>
+          <header className='feedback-comment__info'>
+            <h4>{name}</h4>
+            <p>{username}</p>
+          </header>
+          <div className='feedback-comment__author-image'>
+            <img src={image[0].url} alt={`${name}`} />
+          </div>
+          <div className='feedback-comment__comment'>
+            <p>{comment}</p>
+          </div>
+          <footer className='feedback-comment__reply-button'>Reply</footer>
         </div>
-        <div className='feedback-comment__content'>
-          <p>{comment}</p>
-        </div>
-        <footer className='feedback-comment__reply-button'>Reply</footer>
-        {replyComments().map((c) => (
+        {replyCommentsList().map((reply) => (
           <FeedbackComments
             allComments={allComments}
-            key={c.fields.CommentId}
-            commentId={c.fields.CommentId}
-            name={c.fields.Name}
-            username={c.fields.Username}
-            image={c.fields.Image}
-            comment={c.fields.Comment}
+            key={reply.fields.CommentId}
+            commentId={reply.fields.CommentId}
+            name={reply.fields.Name}
+            username={reply.fields.Username}
+            image={reply.fields.Image}
+            comment={reply.fields.Comment}
             replyComment={true}
           />
         ))}
