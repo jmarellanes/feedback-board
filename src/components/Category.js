d7600cf315bc488c285e47ae0eff1e07a1d6f94a
jmarellanes <dev.jmarellanes@gmail.com>
2021-12-29 05:47:21 -0700

    Fix button not showing active state on clicking in Home

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,15 +4,14 @@ import { useParams } from 'react-router-dom';
 function Category({ category, children, isActive }) {
   const params = useParams();
   const categoryParam = !params.categoryParam ? 'all' : params.categoryParam;
+  category = category.toLowerCase();
 
   return (
     <li className='category__item'>
       <Link
         to={category}
         className={`category__link ${
-          categoryParam === category.toLowerCase()
-            ? 'category__link--active'
-            : ''
+          categoryParam === category ? 'category__link--active' : ''
         } ${!isActive ? 'isInactive' : ''}`}
       >
         {children}
