f8b0efaf56f79319ab6f5cd81243ebb5fb1fc878
jmarellanes <dev.jmarellanes@gmail.com>
2022-01-16 01:58:46 -0700

    Refactor color style in header

--- src/components/HeaderBrandmark.js
+++ src/components/HeaderBrandmark.js
@@ -18,8 +18,8 @@ export default function HeaderBrandmark({ title }) {
 
   return (
     <div className='header-main__brandmark'>
-      <h2 className='header-main__title'>{title}</h2>
-      <div className='header-main__user'>
+      <h2 className='header-main__title header-main__title--light'>{title}</h2>
+      <div className='header-main__user header-main__user--light'>
         {!user ? 'Getting user info' : isUser()}
       </div>
     </div>
