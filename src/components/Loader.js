af7e743f2de7c3778d8ed9e491976bb241b3510f
jmarellanes <dev.jmarellanes@gmail.com>
2022-01-16 02:06:03 -0700

    Refactor loader component
    Change made to cover the case: if FeedbackID doesn't exist, what data should be rendered by Feedback Details component.

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,50 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 function Loader() {
-  return <div className='loader'>Loading...</div>;
+  const [content, setContent] = useState(false);
+
+  const noFeedback = (
+    <>
+      <h2>We're having a problem. There is no Feedback with this ID</h2>
+      <p>Got a suggestion? Found a bug that needs to be squashed?</p>
+      <p>We love hearing about new ideas to improve our app.</p>
+
+      <div className='loader__redirect'>
+        <p>
+          Go to&nbsp;
+          <Link to='/' className='h1'>
+            FeedbackTo
+          </Link>
+          &nbsp;home
+        </p>
+      </div>
+    </>
+  );
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setContent(true);
+    }, 6000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className='loader__container'>
+      <div className='loader__title'>
+        <span className='loader__dots'>Loading...</span>
+      </div>
+      <div
+        className={`loader__content ${content ? 'loader__content--show' : ''}`}
+      >
+        {noFeedback}
+      </div>
+    </div>
+  );
 }
 
 export default Loader;
