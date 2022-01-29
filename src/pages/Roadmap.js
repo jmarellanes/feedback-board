3a0bf141fc2efdfbdd611aa376e3b15a9bebbda0
jmarellanes <dev.jmarellanes@gmail.com>
2022-01-29 05:01:19 -0700

    Fetch DB to get feedback list

--- src/pages/Roadmap.js
+++ src/pages/Roadmap.js
@@ -1,4 +1,27 @@
+import { useState, useEffect } from 'react';
+
 function Roadmap() {
+  const [feedbackPlanned, setFeedbackPlanned] = useState([]);
+  const [feedbackInProgress, setFeedbackInProgress] = useState([]);
+  const [feedbackLive, setFeedbackLive] = useState([]);
+
+  const loadFeedback = async () => {
+    try {
+      const res = await fetch(`/api/getStatus`);
+      const statusRes = await res.json();
+
+      setFeedbackPlanned(statusRes.planned);
+      setFeedbackInProgress(statusRes.inProgress);
+      setFeedbackLive(statusRes.live);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    loadFeedback();
+  }, []);
+
   return <div>Roadmap</div>;
 }
 
