96bbd16354ba91d4ba9a381dbd579aa0c8f8c9ec
jmarellanes <dev.jmarellanes@gmail.com>
2022-01-30 00:51:44 -0700

    Add comment to fn

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,6 @@
+/* =====================================================================
+    -> SHOW ERRORS IN FORMS
+===================================================================== */
 export function errorMessage(obj, name, type, message) {
   return (
     obj[name] && obj[name]?.type === type && <span role='alert'>{message}</span>
