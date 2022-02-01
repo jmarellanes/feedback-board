c90070ad9b5bd5a80eacba785294f929e36ff88d
jmarellanes <dev.jmarellanes@gmail.com>
2022-02-01 03:39:20 -0700

    Add Create Feedback functionality in roadmap page

--- src/pages/Roadmap.js
+++ src/pages/Roadmap.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 
 import FeedbackItem from '../components/FeedbackItem';
+import Modal from '../components/Modal';
+import CreateFeedback from '../components/CreateFeedback';
 import Upvotes from '../components/Upvotes';
 import FeedbackListRoadmap from '../components/FeedbackListRoadmap';
 import Loader from '../components/Loader';
@@ -9,6 +12,8 @@ import Button from '../components/Button';
 
 function Roadmap() {
   const [feedback, setFeedback] = useState([]);
+
+  const [showModal, setShowModal] = useState(false);
   const [loading, setLoading] = useState(true);
 
   const history = useHistory();
@@ -27,33 +32,6 @@ function Roadmap() {
     updateFeedback[index] = updateState;
     setFeedback(updateFeedback);
   };
-
-  const loadFeedback = async (abortCont) => {
-    try {
-      const res = await fetch(`/api/getStatus`, { signal: abortCont.signal });
-      const statusRes = await res.json();
-
-      setFeedback(() => [
-        statusRes.planned,
-        statusRes.inProgress,
-        statusRes.live,
-      ]);
-    } catch (error) {
-      console.log(error);
-    }
-
-    if (!abortCont.signal.aborted) setLoading(false);
-  };
-
-  useEffect(() => {
-    const abortCont = new AbortController();
-
-    loadFeedback(abortCont);
-
-    return () => {
-      abortCont.abort();
-    };
-  }, []);
 
   const feedbackItem = (feedbackData, index) =>
     feedbackData.map((data) => {
@@ -103,25 +81,92 @@ function Roadmap() {
     );
   });
 
+  const closeModal = (e) => {
+    if (e.target.parentNode.hasAttribute('data-operation-running')) return;
+
+    setShowModal(!showModal);
+  };
+
+  const feedbackAdded = () => {
+    setShowModal(!showModal);
+
+    setTimeout(() => {
+      history.push('/');
+    }, 100);
+  };
+
+  const openModal = (
+    <Modal onClose={closeModal} isOpen='modal__is-open'>
+      <CreateFeedback feedbackAdded={feedbackAdded} onClick={closeModal} />
+    </Modal>
+  );
+
+  const loadFeedback = async (abortCont) => {
+    try {
+      const res = await fetch(`/api/getStatus`, { signal: abortCont.signal });
+      const statusRes = await res.json();
+
+      setFeedback(() => [
+        statusRes.planned,
+        statusRes.inProgress,
+        statusRes.live,
+      ]);
+    } catch (error) {
+      console.log(error);
+    }
+
+    if (!abortCont.signal.aborted) setLoading(false);
+  };
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+
+    loadFeedback(abortCont);
+
+    return () => {
+      abortCont.abort();
+    };
+  }, []);
+
   return (
-    <div id='roadmap-page__wrapper'>
-      <header className='header-tertiary'>
-        <div className='header-tertiary__container'>
-          <Button
-            typeAttribute='button'
-            buttonStyle='button--back-dark'
-            svgIcon='chevron-left'
-            onClick={() => history.goBack()}
-          >
-            Go Back
-          </Button>
-        </div>
-        <span>Placeholder for Top Bar</span>
-      </header>
-      <main className='roadmap-page roadmap-page__content'>
-        {loading ? <Loader type='feedback-roadmap' /> : feedbackList}
-      </main>
-    </div>
+    <>
+      <div id='roadmap-page__wrapper'>
+        <header className='header-tertiary'>
+          <div className='header-tertiary__container'>
+            <Button
+              typeAttribute='button'
+              buttonStyle='button--back-dark'
+              svgIcon='chevron-left'
+              onClick={() => history.goBack()}
+            >
+              Go Back
+            </Button>
+            <Button
+              typeAttribute='button'
+              buttonStyle='button--primary'
+              svgIcon='sign'
+              onClick={() => setShowModal(!showModal)}
+            >
+              Add Feedback
+            </Button>
+          </div>
+          <span>Placeholder for Top Bar</span>
+        </header>
+        <main className='roadmap-page roadmap-page__content'>
+          {loading ? <Loader type='feedback-roadmap' /> : feedbackList}
+        </main>
+      </div>
+
+      {/* Add Feedback Modal*/}
+      <CSSTransition
+        in={showModal}
+        classNames='modal-transition'
+        unmountOnExit
+        timeout={250}
+      >
+        {openModal}
+      </CSSTransition>
+    </>
   );
 }
 
