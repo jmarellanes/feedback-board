7f8cff3362f9b293981b9b3da4075c6cdcaf7046
jmarellanes <dev.jmarellanes@gmail.com>
2022-04-07 03:17:27 -0600

    Stop event in <a> if is the active category

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,6 +4,11 @@ function Category({ category, isActive }) {
   const { categoryParam } = useParams();
   const cat = category.toLowerCase();
 
+  const stopEvent = (e) => {
+    if (e.target.classList.contains('category__link--active'))
+      e.preventDefault();
+  };
+
   return (
     <li className='category__item'>
       <Link
@@ -11,6 +16,7 @@ function Category({ category, isActive }) {
         className={`category__link ${
           categoryParam === cat ? 'category__link--active' : ''
         } ${!isActive ? 'isInactive' : ''}`}
+        onClick={stopEvent}
       >
         {cat}
       </Link>
