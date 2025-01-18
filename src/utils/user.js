1de56a415eeff5bd015c080fc19f60e321ed53c8
José Manuel Arellanes <dev@josearellanes.me>
2025-01-18 00:56:17 -0600

    fix: update user profile image URL
    Replaces the Airtable-hosted user profile image URL with a
    new URL hosted on Cloudinary to ensure better reliability
    and access.
    
    This change does not impact functionality, only updates
    the resource location.

--- src/utils/user.js
+++ src/utils/user.js
@@ -4,6 +4,6 @@ export const login = async () => {
     name: 'Zena Kelley',
     username: 'velvetround',
     image:
-      'https://dl.airtable.com/.attachmentThumbnails/71d8e90a6c173688f3f0c53711a2e4c5/06bf91c0',
+      'https://res.cloudinary.com/jmarellanes/image/upload/v1737183243/PaNurkvwWQbjSY04El4cFh5HlMeTygKOYDuUs7_07-8_yu1joe.jpg',
   };
 };
